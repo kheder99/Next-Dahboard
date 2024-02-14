f1d6a0a66c28c1f37674d0fe1850c86dc88da9a8
kheder99 <khederibrahem99@gmail.com>
2024-02-15 02:10:54 +0300

    final project

--- auth.config.ts
+++ auth.config.ts
@@ -8,18 +8,18 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith(
-        'https://next-dahboard-sepia.vercel.app/',
+        'https://next-dahboard-sepia.vercel.app',
       );
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        console.log(new URL('/dashboard', nextUrl));
-        // new URL('https://next-dahboard-sepia.vercel.app/', nextUrl),
-        return Response.redirect(
-          new URL('/dashboard', 'https://next-dahboard-sepia.vercel.app/'),
-        );
-      }
+      // if (isOnDashboard) {
+      //   if (isLoggedIn) return true;
+      //   return false; // Redirect unauthenticated users to login page
+      // } else if (isLoggedIn) {
+      //   console.log(new URL('/dashboard', nextUrl));
+      //   // new URL('https://next-dahboard-sepia.vercel.app/', nextUrl),
+      //   return Response.redirect(
+      //     new URL('/dashboard', 'https://next-dahboard-sepia.vercel.app'),
+      //   );
+      // }
       return true;
     },
   },
